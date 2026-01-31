5377ab697261934289043ffe96cb590d17b177de
rishabh4614 <rishabhr00j@gmail.com>
2026-02-01 01:45:59 +0530

    spell Pages make to upper case

--- src/Router/AppRoutes.jsx
+++ src/Router/AppRoutes.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ScrollToTop from "../Components/ScrollToTop"; // 👈 Import this
 
-import Home from "../pages/Home";
+import Home from "../Pages/Home";
 import About from "../Pages/About";
-import Contact from "../pages/Contact";
+import Contact from "../Pages/Contact";
 import NotFound from "../Pages/NotFound";
 import Header from "../Components/Header/Navbar";
 import Footer from "../Components/Footer/Footer";
