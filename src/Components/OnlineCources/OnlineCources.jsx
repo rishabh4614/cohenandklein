13b6a6a0df6268bd74e94614df6655af6d6e6ecb
rishabh4614 <rishabhr00j@gmail.com>
2026-04-18 18:14:20 +0530

    font size reduced

--- src/Components/OnlineCources/OnlineCources.jsx
+++ src/Components/OnlineCources/OnlineCources.jsx
@@ -28,7 +28,7 @@ const OnlineCources = () => {
               className="font-serif lg:text-[36px] sm:text-2xl text-base text-center text-primary font-semibold"
               onClick={() => window.open(CoursePdf.pdf202612, "_blank")}
             >
-              <h1 className="text-primary 2xl:text-[54px] lg:text-4xl 2xl:leading-[53px] sm:text-3xl text-base text-center roboto-serif-font font-semibold break-words">
+              <h1 className="text-primary 2xl:text-[40px] lg:text-4xl 2xl:leading-[40px] sm:text-3xl text-base text-center roboto-serif-font font-semibold break-words">
                 2026, Debt Collections and Revenue Management Virtual Training
                 Schedule - Zoom Platform
               </h1>
